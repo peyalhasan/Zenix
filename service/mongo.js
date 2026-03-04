a65b6cd5cdfebe39bfb7107459d025ab0df1f68c
Peyal Hasan <peyalhasan143@gmail.com>
2026-03-04 16:28:22 +0600

    update Mongo

--- service/mongo.js
+++ service/mongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export async function connectMongo(){
     try{
-        const conn = await mongoose.connect(String(process.env.MONGO_URI))
+        const conn = await mongoose.connect(String(process.env.MONGODB_URI))
         return conn;
     }catch(err){
         console.error(err.message)
