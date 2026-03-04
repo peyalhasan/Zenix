ccb6b3c071fb456f047b29a2b0461147cd1dd75a
Peyal Hasan <peyalhasan143@gmail.com>
2026-03-04 15:58:27 +0600

    Sloved Error

--- components/hotel/details/Gallery.jsx
+++ components/hotel/details/Gallery.jsx
@@ -1,14 +1,22 @@
+import Image from "next/image";
+
 const Gallery = () => {
   return (
     <section className="container">
       <div className="grid grid-cols-2 imageshowCase ">
-        <img src="./assets/images/1.png" className="h-[400px] border" alt="" />
+        <Image
+          src="/image.jpg"
+          alt="hotel"
+          width={400}
+          height={300}
+          className="border"
+        />
 
         <div className="grid grid-cols-2 grid-rows-2 h-[400px]">
-          <img src="./assets/images/2.png" alt="" className="border" />
-          <img src="./assets/images/3.png" alt="" className="border" />
-          <img src="./assets/images/4.png" alt="" className="border" />
-          <img src="./assets/images/5.png" alt="" className="border" />
+          <Image src="/image.jpg" alt="hotel" width={400} height={300}   className="border" />
+          <Image src="/image.jpg" alt="hotel" width={400} height={300}   className="border" />
+          <Image src="/image.jpg" alt="hotel" width={400} height={300}   className="border" />
+          <Image src="/image.jpg" alt="hotel" width={400} height={300}   className="border" />
         </div>
       </div>
     </section>
